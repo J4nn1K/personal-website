5ad63b56649df961ec39a594e5e4424c9350c5a9
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-05-14 21:43:33 +0200

    Resolve ESLint error

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div class="container mx-auto p-4">
-      <h1 class="text-5xl font-black">
+    <div className="container mx-auto p-4">
+      <h1 className="text-5xl font-black">
         Jannik Jorge Grothusen
       </h1>
-      <a class="text-2xl font-normal">
+      <a className="text-2xl font-normal">
         Making what I want since 2000.
       </a>
     </div>
