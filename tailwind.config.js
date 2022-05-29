d0a5df5a8cfd056e6b6e82c8d99c377a5545d3a6
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-05-29 22:50:20 +0200

    Change font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ['Inter', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {},
+    fontFamily: {
+      sans: ['Inter', 'Helvetica', 'sans-serif'],
+    }
   },
   plugins: [
     require("daisyui")
