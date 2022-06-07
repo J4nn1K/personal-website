4ab6d436e800b216d239a7df4bc3bc51e2e20d5c
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-06-07 23:29:12 +0200

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
     extend: {},
     fontFamily: {
       sans: ['Inter', 'Helvetica', 'sans-serif'],
+    },
+    colors: {
+      gray: {
+        100: '#F5F5F5',
+        200: '#D8D8D8',
+        300: '#BBBBBB',
+        400: '#9E9E9E',
+        500: '#818181',
+        600: '#646464',
+        700: '#474747',
+        800: '#2A2A2A',
+        900: '#0D0D0D',
+      }
     }
   },
   plugins: [
